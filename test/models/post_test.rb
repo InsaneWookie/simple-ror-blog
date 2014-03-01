68dd4f498757b215fd99834dcf0df3f1f0c2a767
Rowan <rowan.tate@gmail.com>
2014-03-01 20:43:11 +1300

    Added some model tests

--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -1,7 +1,19 @@
 require 'test_helper'
 
 class PostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should not save post without title" do
+    post = Post.new
+    assert !post.save
+  end
+  
+  
+  test "should delete comments with post" do
+    post = posts(:one)
+    #post = Post.find(1)
+    assert post.comments.count > 0, "no comments found"
+    
+    post.destroy
+    
+    assert post.comments.count == 0, "comments still exist"
+  end
 end
