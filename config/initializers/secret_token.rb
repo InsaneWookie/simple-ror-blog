b0fdcf31121ad8910b3bd30c617b9aa98ca04a42
Rowan <rowan.tate@gmail.com>
2014-03-01 16:43:10 +1300

    inital commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SleepySavannah7126::Application.config.secret_key_base = 'b8713a5dafa9662a05345466a8e5f07c2b228f3f16daeda8834fa942b0235387f5f0d55dfddd072ff990880e2a2c5eb31c6d05a3c5cdaf5efa4a69f7346ae9c4'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SleepySavannah7126::Application.config.session_store :cookie_store, key: '_sleepy-savannah-7126_session'
